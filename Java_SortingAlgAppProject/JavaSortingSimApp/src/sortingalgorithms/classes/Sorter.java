3a8551ae130e2ce5a2a1d7ac2536e53398e7e95b
CRdevcoder <chris.rodriguez8123@gmail.com>
2026-08-05 19:41:17 -0400

    Sorter interface extending  Subject Interface.

--- Java_SortingAlgAppProject/JavaSortingSimApp/src/sortingalgorithms/classes/Sorter.java
+++ Java_SortingAlgAppProject/JavaSortingSimApp/src/sortingalgorithms/classes/Sorter.java
@@ -1,12 +1,22 @@
 package sortingalgorithms.classes;
 import java.util.ArrayList;
 import java.lang.Comparable;
+import utility.classes.Subject;
 
 // NOTE: Sorter Interface for algorithms.
-public interface Sorter <T extends Comparable<T>> {
+public interface Sorter <T extends Comparable<T>> extends Subject<Sorter.SortingEvent> {
 
     // Takes an ArrayList, copies it, sorts it, then returns the sorted list.
     // Accepts DataTypes that implement Comparable Interface.
     public ArrayList<T> sortList(ArrayList<T> list);
+
+    public enum SortingEvent {
+        REDRAW_ARRAY_PLAIN, // redraw the array and keep all bars the same color.
+        ELEMENT_SELECTED, // highlight the selected element in the array.
+        ELEMENT_MOVED,
+        ELEMENT_SWAPPED
+    }
+
+    // Idea: get selected index method. Returns index that is modified/swapped. This will be used to highlight the element in the GUI.
 
 }
