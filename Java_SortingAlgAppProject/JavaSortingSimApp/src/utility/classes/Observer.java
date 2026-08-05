6af727d271d3bac5c49103025a71c82268c9aca7
CRdevcoder <chris.rodriguez8123@gmail.com>
2026-08-05 19:21:10 -0400

    Observer and Subject update. Removed T generic parameter from Observer Interface.

--- Java_SortingAlgAppProject/JavaSortingSimApp/src/utility/classes/Observer.java
+++ Java_SortingAlgAppProject/JavaSortingSimApp/src/utility/classes/Observer.java
@@ -2,12 +2,11 @@ package utility.classes;
 
 import utility.classes.Subject;
 
-// Paramter T is the class type of the subject that the observer is observing.
 // Parameter E is the enum type of the event that the subject is notifying this observer of.
-public interface Observer <T extends Subject, E extends Enum<E>> {
+public interface Observer <E extends Enum<E>> {
 
     // subjects call this method to notify their observers. They send their reference too.
     // And uses enums to categorize the type of event that is being notified to the observer.
-    public void onNotify(T subject, E eventType);
+    public void onNotify( Subject<E> subject, E eventType);
 
 }
--- Java_SortingAlgAppProject/JavaSortingSimApp/src/utility/classes/Subject.java
+++ Java_SortingAlgAppProject/JavaSortingSimApp/src/utility/classes/Subject.java
@@ -2,15 +2,16 @@ package utility.classes;
 
 import utility.classes.Observer;
 
+// E is the enum type of the event that the subject is notifying this observer of.
 public interface Subject<E extends Enum<E>> {
 
     // use hashtables to store observers? No.
 
     // add given observer to the list of observers.
-    public void addObserver(Observer<? extends Subject<E>,E> observer);
+    public void addObserver(Observer<E> observer);
 
     // remove given observer from the list of observers.
-    public void removeObserver(Observer<? extends Subject<E>,E> observer);
+    public void removeObserver(Observer<E> observer);
 
     // iterate through each observer and call their onNotify method.
     public void notifyObservers();
