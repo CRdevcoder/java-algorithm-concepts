d3d2a48ddbdaf345765f316cfd480a1a0443e12c
CRdevcoder <chris.rodriguez8123@gmail.com>
2026-08-08 12:48:12 -0400

    New Class - GraphListDisplay - Tested GraphListDisplay inside AnimManagerTest.java

---
+++ Java_SortingAlgAppProject/JavaSortingSimApp/src/gui/GraphListDisplay.java
@@ -0,0 +1,48 @@
+package gui;
+
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+// a gui that displays a list of graphs.
+// can add or remove graphs from the list.
+public class GraphListDisplay extends JPanel {
+
+    private ArrayList<BarGraphGui> graphList;
+
+    public GraphListDisplay() {
+        this.graphList = new ArrayList<>();
+
+        // verticle layout for the list of graphs.
+        BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        this.setLayout(layout);
+
+    }
+
+    // add a graph to the list.
+    public void addGraph(BarGraphGui graph) {
+        this.graphList.add(graph);
+        this.add(graph); // add the graph to the JPanel.
+        this.revalidate(); // revalidate the JPanel to update the layout.
+        this.repaint(); // repaint the JPanel to update the display.
+    }
+
+    public void addGraph( AnimationManager animManager) {
+        BarGraphGui graph = animManager.getBarGraphGui();
+        this.addGraph(graph);
+    }
+
+    // remove a graph from the list.
+    public void removeGraph(BarGraphGui graph) {
+        this.graphList.remove(graph);
+        this.remove(graph); // remove the graph from the JPanel.
+        this.revalidate(); // revalidate the JPanel to update the layout.
+        this.repaint(); // repaint the JPanel to update the display.
+    }
+
+    public void removeGraph(AnimationManager animManager){
+        BarGraphGui graph = animManager.getBarGraphGui();
+        this.removeGraph(graph);
+    }
+    
+}
--- Java_SortingAlgAppProject/JavaSortingSimApp/src/gui/tests/AnimManagerTest.java
+++ Java_SortingAlgAppProject/JavaSortingSimApp/src/gui/tests/AnimManagerTest.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 
 import gui.AnimationManager;
 import gui.AppWindow;
+import gui.GraphListDisplay;
 import sortingalgorithms.classes.BubbleSort;
 import sortingalgorithms.classes.MergeSort;
 import utility.classes.NumArrayGenerator;
@@ -24,19 +25,26 @@ public class AnimManagerTest {
         NumArrayGenerator ng = new NumArrayGenerator(237, 1,30);
 
         // create focus array.
-        ArrayList<Integer> focusArray1 = new ArrayList<>(Arrays.asList(20,1,10,3,8,2,4,5,6));
+        ArrayList<Integer> focusArray1 = new ArrayList<>(Arrays.asList(20,1,10,3,8,2,4,5,6,8,15,4,7));
         ArrayList<Integer> focusArray2 = ng.generateIntList(20);
 
         // sorters
         BubbleSort<Integer> bub1 = new BubbleSort<>();
-        MergeSort<Integer> bub2 = new MergeSort<>();
+        MergeSort<Integer> merge1 = new MergeSort<>();
+
+        // Jpanel gui.
+        GraphListDisplay graphListDisplay = new GraphListDisplay();
 
         // create animation manager.
         AnimationManager animManager1 = new AnimationManager(focusArray1, bub1,500);
         // 2nd one.
-        AnimationManager animManager2 = new AnimationManager(focusArray2, bub2,500);
+        AnimationManager animManager2 = new AnimationManager(focusArray2, merge1,500);
         // set bar color to green.
         animManager2.setGraphColor(Color.GREEN);
+
+        // Add the bar graph guis into the graph display.
+        graphListDisplay.addGraph(animManager1);
+        graphListDisplay.addGraph(animManager2);
 
         AppWindow window = new AppWindow("Animation Manager Test", 800, 600, true);
 
@@ -46,8 +54,8 @@ public class AnimManagerTest {
 
         window.add(mainContainer);
         // add bar graph guis to main container.
-        mainContainer.add(animManager1.getBarGraphGui());
-        mainContainer.add(animManager2.getBarGraphGui());
+        mainContainer.add(graphListDisplay);
+        //mainContainer.add(animManager2.getBarGraphGui());
 
         // execute the sorting algorithms in seperate threads to run both animations asynchronously.
         ExecutorService executor = Executors.newFixedThreadPool(2);
@@ -55,7 +63,7 @@ public class AnimManagerTest {
         // begin sorter.
         // Try executing these in seperate threads,so the thread delays dont block each other.
         executor.execute(() -> bub1.sortList(focusArray1));
-        executor.execute(() -> bub2.sortList(focusArray2));
+        executor.execute(() -> merge1.sortList(focusArray2));
 
         executor.shutdown();
     }
