822ee6be3296707b8440f74531418d4ff1127cc0
CRdevcoder <chris.rodriguez8123@gmail.com>
2026-08-13 15:06:31 -0400

    Update- Sorter Interface extends Subject<SortingEvent> Original SortingEvent Enums have been completely removed.

--- Java_SortingAlgAppProject/JavaSortingSimApp/src/sortingalgorithms/classes/Sorter.java
+++ Java_SortingAlgAppProject/JavaSortingSimApp/src/sortingalgorithms/classes/Sorter.java
@@ -1,23 +1,24 @@
 package sortingalgorithms.classes;
 import java.util.ArrayList;
 
+import observerpattern.SortingEvent;
 import observerpattern.Subject;
 
 import java.lang.Comparable;
 
 // NOTE: Sorter Interface for algorithms.
-public interface Sorter <T extends Comparable<T>> extends Subject<Sorter.SortingEvent> {
+public interface Sorter <T extends Comparable<T>> extends Subject<SortingEvent> {
 
     // Takes an ArrayList, copies it, sorts it, then returns the sorted list.
     // Accepts DataTypes that implement Comparable Interface.
     public ArrayList<T> sortList(ArrayList<T> list);
 
-    public enum SortingEvent {
-        REDRAW_ARRAY_PLAIN, // redraw the array and keep all bars the same color.
-        ELEMENT_SELECTED, // highlight the selected element in the array.
-        ELEMENT_MOVED,
-        ELEMENT_SWAPPED
-    }
+    //public enum SortingEvent {
+    //    REDRAW_ARRAY_PLAIN, // redraw the array and keep all bars the same color.
+    ///    ELEMENT_SELECTED, // highlight the selected element in the array.
+    //    ELEMENT_MOVED,
+    //    ELEMENT_SWAPPED
+    //}
 
     // Idea: get selected index method. Returns index that is modified/swapped. This will be used to highlight the element in the GUI.
 
