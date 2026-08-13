6b7f8ffa86e4b106acc8470a60d605e829bbbc9b
CRdevcoder <chris.rodriguez8123@gmail.com>
2026-08-13 15:19:49 -0400

    Update-BarGraphGui implemented new bar coloring system using Hashtables. Now stores data that tells it how to draw the graph at any given momment. AnimationUnit uses special functions to update its drawing data. New Attributes: private Map<ColorType,Color> colorLibrary; private Map<Integer, ColorType> selectionData; public enum ColorType: MOVED,SCANNED,IDLE. public void setSelectedBars(int[] indexes, ColorType color)

--- Java_SortingAlgAppProject/JavaSortingSimApp/src/gui/BarGraphGui.java
+++ Java_SortingAlgAppProject/JavaSortingSimApp/src/gui/BarGraphGui.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
 
 import javax.swing.JPanel;
 
@@ -14,22 +16,78 @@ public class BarGraphGui extends JPanel {
     // array we are drawing.
     private ArrayList<Integer> focusArray;
 
-    private Color barColor;
+    // store indexes that are being read or moved by algorithm.
+    // these indexes will have bars that are colored differently from the unaccessed ones.
+    // NOTE: AnimationUnit will provide a reference to a hashset for this variable.
+    // Hashset will be modified externally (???)
+    private Map<Integer, ColorType> selectionData;
+
+    // associates each color with a enum.
+    // Can't retreive reference to it. Only set values.
+    private Map<ColorType,Color> colorLibrary;
 
     // Constructor
     public BarGraphGui(ArrayList<Integer> focusArray) {
         this.focusArray = focusArray;
-        this.barColor = Color.BLUE; // default color
-        
+
+        // stores colors we use.
+        this.colorLibrary = new Hashtable<>();
+
+        colorLibrary.put(ColorType.IDLE, Color.WHITE); // default color
+        colorLibrary.put(ColorType.MOVED, Color.RED); // when moving an element.
+        colorLibrary.put(ColorType.SCANNED, Color.CYAN); // when searching element.
+
+        // create hashset for selected data.
+        this.selectionData = new Hashtable<Integer,ColorType>();
+
+        // set background.
+        this.setBackground(Color.BLACK);
     }
 
+    // Setters
     public void setFocusArray(ArrayList<Integer> focusArray) {
         this.focusArray = focusArray;
     }
 
     public void setBarColor(Color color) {
-        this.barColor = color;
+        // update map for IDLE mode.
+        this.colorLibrary.put(ColorType.IDLE, color);
     }
+    
+    // Getters.
+    // PRIVATE: pass ColorType as key. Retreive corresponding Color object from map.
+    private Color getColorFromType( ColorType type){
+        return colorLibrary.get(type);
+    }
+
+    // COLORING SELECTED ELEMENTS IMPLEMENTATION:
+    // must create private map to associate enums with colors.
+    public enum ColorType{
+        MOVED,  // for when elements are being moved.
+        SCANNED, // for when elemnts are being compared or "scanned".
+        IDLE // for untouched data
+    }
+    
+    // Assign object reference to hashtable variable.
+    public void setSelectionData( Hashtable<Integer,ColorType> selectionData){
+        this.selectionData = selectionData;
+    }
+
+    // set what bars are selected within array.
+    // They will be colored differently.
+    // AnimationUnit uses this to update graph and pass data to it.
+    public void setSelectedBars(int[] indexes, ColorType color) {
+        for (int index : indexes) {
+            this.selectionData.put( Integer.valueOf(index),color);
+        }
+    }
+
+    // Will be called by AnimationUnit every time its notified.
+    // use to clear index data and make room for new data.
+    public void clearSelectionData(){
+        this.selectionData.clear();
+    }
+
 
     // call repaint() to update the bar graph when the array changes.
     @Override
@@ -58,6 +116,10 @@ public class BarGraphGui extends JPanel {
     }
 
     // update and draw bar graph.
+    // 1. calculate delta x and y for dimensions of rectangles.
+    // 2. iterate through each array element.
+    // 3. If index has corresponding ColorType value inside selectionData, get it.
+    // 4. Pass that very ColorType enum value to the getColorFromType to get the color.
     public void drawState(Graphics2D g2) 
 	{
 		// divides the width of the canvas component width by the array length.
@@ -65,13 +127,32 @@ public class BarGraphGui extends JPanel {
 		int deltaX = this.getWidth() / focusArray.size();
 		int deltaY = (this.getHeight()/3) / focusArray.size();
 
-
-	    for (int i = 0; i < focusArray.size(); i++)
-		{   
+        // if no hashset assigned, or hashset is empty, color all bars the same.
+        if ((selectionData == null) || (selectionData.isEmpty()))
+        {
+	        for (int i = 0; i < focusArray.size(); i++)
+		    {   
             // draw bar.
-            drawIndexBar(i, deltaX, deltaY, this.barColor, g2);
-	    }
-    }
+            drawIndexBar(i, deltaX, deltaY, getColorFromType(ColorType.IDLE), g2);
+	        }
+        }
+        else{ // else, then color each selected element differently.
+
+            for (int i = 0; i < focusArray.size(); i++)
+            {
+                // draw selected bar.
+                if(selectionData.containsKey(i)){
+                    // get proper color from map.
+                    drawIndexBar(i, deltaX, deltaY, getColorFromType(selectionData.get(i)), g2);
+                }
+                else{
+                    drawIndexBar(i, deltaX, deltaY, getColorFromType(ColorType.IDLE), g2);
+                }
+            }
+
+        }
+    } 
+
 
     // prints focus array for bar graph.
     @Override
